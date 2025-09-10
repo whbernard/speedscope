68177a35c66ca4f375be645ca6baf0b98b29cdd9
William Bernard <whb@teleapp.com>
2025-09-10 11:11:46 -0400

    chore: remove CI script

--- scripts/ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euxo pipefail
-
-npm run typecheck
-npm run lint
-npm run coverage
-# Skip schema generation due to TypeScript version incompatibility with @types packages
-# node scripts/generate-file-format-schema-json.js > /dev/null
